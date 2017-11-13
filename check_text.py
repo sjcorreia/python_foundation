132092a5081f9a2dccecf2d7db61e6c64898c3b9
cos1hi <external.steven.correia@de.bosch.com>
2017-11-13 14:12:42 +0100

    Initial check, while iterating over each word

--- check_text.py
+++ check_text.py
@@ -1,10 +1,35 @@
 from __future__ import print_function
+import urllib.request
+# urllib.request is the module needed for Python 3.4.3
+# perhaps with Python 2.7 there is a different way to use urllib
 
 def read_test():
-	quotes = open("movie_quotes.txt")
+	quotes = open("text/movie_quotes.txt")
 	contents = quotes.read()
-	print(contents)
-	# wdyl.com/profamity?q=
+	# print(contents)
+	# wdyl.com/profanity?q=
 	# http://www.wdylike.appspot.com/?q=shot
+	
+    # Test this again without iterating over each word in the list!!
+    # Think about performance, but at least this works
+	contentList = contents.split()
+	quotes.close()
+	for word in contentList:
+	    check_profanity(word)
+
+def check_profanity(text_to_check):
+	# print(text_to_check)
+	proxy_settings = urllib.request.getproxies()
+	# urllib.request.ProxyHandler(proxy_settings)
+	url_text = "http://www.wdylike.appspot.com/?q="
+	new_url = url_text + text_to_check
+	# print(new_url)
+	# url_text = r"http://www.wdylike.appspot.com/?q=shot"
+	response = urllib.request.urlopen(new_url)
+	output = response.read()
+	if output == b'true':
+	    print("Profanity detected!!")
+	# print(output)
+	response.close()
 
 read_test()
